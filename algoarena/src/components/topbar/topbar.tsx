a6f9ceec540140f069f73864b0cc44ec147d38f9
Pratyushs411 <pratyushksk@gmail.com>
2024-10-22 11:19:28 +0530

    auth update on topbar

--- algoarena/src/components/topbar/topbar.tsx
+++ algoarena/src/components/topbar/topbar.tsx
@@ -2,15 +2,21 @@ import { auth } from "@/firebase/firebase";
 import Link from "next/link";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+//import Logout from "../buttons/logout";
 import { useSetRecoilState } from "recoil";
 import { authModalState } from "@/atoms/authModalAtom";
 import Image from "next/image";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { BsList } from "react-icons/bs";
+import { useRouter } from "next/router";
 type topbarProps = {
     
 };
 
 const topbar:React.FC<topbarProps> = () => {
-    
+    const [user] = useAuthState(auth);
+	const setAuthModalState = useSetRecoilState(authModalState);
+	const router = useRouter();
     return (
         <nav className='relative flex h-[60px] w-full shrink-0 items-center justify-between px-5 bg-white text-black shadow-md'>
         <div className='flex w-full items-center justify-between max-w-[1200px] mx-auto'>
@@ -38,13 +44,28 @@ const topbar:React.FC<topbarProps> = () => {
 				>
 					Discuss
 				</button>
-                <Link href='/auth'>
-				<button className='bg-turquoise text-white px-2 py-1 sm:px-4 rounded-md text-md 
+                {!user && (
+						<Link
+							href='/auth'
+							onClick={() => setAuthModalState((prev) => ({ ...prev, isOpen: true, type: "login" }))}
+						>
+							<button className='bg-turquoise text-white px-2 py-1 sm:px-4 rounded-md text-md 
                 hover:text-turquoise hover:bg-white hover:border-2 hover:border-turquoise border-2 border-transparent
-                transition duration-300 ease-in-out
-                '
-				>Sign In</button>
-				</Link>
+                transition duration-300 ease-in-out'>Sign In</button>
+						</Link>
+					)}
+					{user && (
+						<div className='cursor-pointer group relative'>
+							<Image src='/avatar.png' alt='Avatar' width={30} height={30} className='rounded-full' />
+							<div
+								className='absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-white text-turquoise p-2 rounded shadow-lg 
+								z-40 group-hover:scale-100 scale-0 
+								transition-all duration-300 ease-in-out border border-gray-200'
+							>
+								<p className='text-sm'>{user.email}</p>
+							</div>
+						</div>
+					)}
 			</div>
         </div>
       </nav>      
