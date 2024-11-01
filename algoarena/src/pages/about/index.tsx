ba9107a08169132189c4cd2fd2615fb51d1b239b
Pratyushs411 <pratyushksk@gmail.com>
2024-11-01 18:25:25 +0530

    about us done

--- algoarena/src/pages/about/index.tsx
+++ algoarena/src/pages/about/index.tsx
@@ -3,23 +3,36 @@ import React, { useState, useEffect } from 'react';
 import Navbar from '@/components/topbar/topbar'; // Make sure the Navbar is correctly imported
 import Footer from '@/components/footer/footer'; // Importing Footer component
 import styles from './about.module.css';
+import { FaGithub ,FaLinkedin } from 'react-icons/fa';
 // Team Members Data
 const teamMembers = [
   {
     name: 'Pratyush Sharma',
-    description: 'Pratyush is an innovative interior designer who excels at transforming spaces into personalized havens. His attention to detail and commitment to quality are unmatched.',
+    role: 'Developer/Designer',
+    github : 'https://github.com/Pratyushs411',
+    linkedin : 'https://www.linkedin.com/in/pratyushsharma411/',
+    description: 'Pratyush is a skilled developer and designer who combines technical expertise with creative flair to build engaging and user-friendly websites.',
   },
   {
     name: 'Somil Kumar',
-    description: 'Somil is a visionary architect with over a decade of experience in creating stunning residential spaces. His passion lies in blending functionality with aesthetics.',
+    role: 'Designer',
+    github : 'https://github.com/ksomil2562',
+    linkedin : 'https://www.linkedin.com/in/somil-kumar-4307a0258/',
+    description: 'Somil is a talented designer dedicated to crafting visually appealing and intuitive designs,engaging user experience.'
   },
   {
     name: 'Dilpreet Singh',
-    description: 'Dilpreet is a seasoned realtor with a deep understanding of the local market. He is dedicated to finding clients their dream homes while ensuring a smooth buying experience.',
+    role: 'Documentation',
+    github : '#',
+    linkedin:'https://www.linkedin.com/in/dilpreet-singh-4742031b6/',
+    description: 'Dilpreet is a versatile designer and documentation specialist who combines creativity with clarity.',
   },
   {
     name: 'Ruhani Grover',
-    description: 'Ruhani is a talented project manager who brings designs to life. Her organizational skills and keen eye for design help in delivering projects on time and within budget.',
+    role: 'Documentation',
+    github : '#',
+    linkedin:'#',
+    description: 'Ruhani is a dedicated documentation specialist who excels in creating clear and concise materials.',
   },
 ];
 const gifs = [
@@ -56,7 +69,7 @@ const AboutPage: React.FC = () => {
         <div className="w-1/2">
           <h1 className="text-4xl font-bold text-turquoise mb-6">About Us</h1>
           <p className="text-gray-600 text-lg leading-relaxed text-justify">
-          Welcome to AlgoArena, a dynamic and inclusive coding platform built to fuel your passion for programming, hone your skills, and connect you with a vibrant community of learners. AlgoArena is the right place for anybody who wants to start with the world of programming and software development. AlgoArena will provide you with structured content with ample practice problems to solidify your knowledge. Our team is committed to keeping AlgoArena at the forefront of coding education and career development. We regularly update our content, features, and community resources to reflect the latest industry trends and standards.
+            Welcome to AlgoArena, a dynamic and inclusive coding platform built to fuel your passion for programming, hone your skills, and connect you with a vibrant community of learners. AlgoArena is the right place for anybody who wants to start with the world of programming and software development. AlgoArena will provide you with structured content with ample practice problems to solidify your knowledge. Our team is committed to keeping AlgoArena at the forefront of coding education and career development. We regularly update our content, features, and community resources to reflect the latest industry trends and standards.
           </p>
         </div>
 
@@ -85,12 +98,12 @@ const AboutPage: React.FC = () => {
         <div className="w-1/2">
           <h1 className="text-4xl font-bold text-turquoise mb-6">Our Mission</h1>
           <p className="text-gray-600 text-lg leading-relaxed text-justify">
-          Our mission is to empower developers at all skill levels by offering an inclusive platform for engaging, real-world coding competitions that foster continuous growth, creativity, and technical excellence. We aim to bring together a global community of coders, creating opportunities to learn, innovate, and collaborate across diverse backgrounds. By providing fair, accessible challenges, we strive to nurture future-ready skills, prepare coders for impactful careers, and inspire a tech landscape that values skill, passion, and potential. Through every contest, we champion diversity, resilience, and a shared commitment to innovation.
+            Our mission is to empower developers at all skill levels by offering an inclusive platform for engaging, real-world coding competitions that foster continuous growth, creativity, and technical excellence. We aim to bring together a global community of coders, creating opportunities to learn, innovate, and collaborate across diverse backgrounds. By providing fair, accessible challenges, we strive to nurture future-ready skills, prepare coders for impactful careers, and inspire a tech landscape that values skill, passion, and potential. Through every contest, we champion diversity, resilience, and a shared commitment to innovation.
           </p>
         </div>
 
         {/* Right side - Image */}
-      
+
       </section>
 
       {/* Separator */}
@@ -101,29 +114,27 @@ const AboutPage: React.FC = () => {
         <div className="w-1/2 pl-12">
           <h1 className="text-4xl font-bold text-teal-500 mb-6">Our Goal</h1>
           <p className="text-gray-600 text-lg leading-relaxed text-justify">
-          At AlgoArena, our primary goal is to democratize coding education and make it accessible to everyone, regardless of their background or experience level. We strive to create a comprehensive learning environment that fosters growth, creativity, and collaboration among aspiring and experienced developers alike. By offering diverse resources, including coding challenges, interactive tutorials, and community support, we aim to enhance problem-solving skills and technical knowledge. In addition to skill development, we are committed to helping individuals land their dream jobs at leading tech companies. We provide tailored resources such as mock interviews, resume building, and career coaching to bridge the gap between education and industry demands. Our vision is to cultivate a global community where knowledge is shared, innovation thrives, and every member feels empowered to pursue their coding journey with confidence, ultimately achieving their professional aspirations.
+            At AlgoArena, our primary goal is to democratize coding education and make it accessible to everyone, regardless of their background or experience level. We strive to create a comprehensive learning environment that fosters growth, creativity, and collaboration among aspiring and experienced developers alike. By offering diverse resources, including coding challenges, interactive tutorials, and community support, we aim to enhance problem-solving skills and technical knowledge. In addition to skill development, we are committed to helping individuals land their dream jobs at leading tech companies. We provide tailored resources such as mock interviews, resume building, and career coaching to bridge the gap between education and industry demands. Our vision is to cultivate a global community where knowledge is shared, innovation thrives, and every member feels empowered to pursue their coding journey with confidence, ultimately achieving their professional aspirations.
           </p>
         </div>
         <div className="container mx-auto px-6 flex items-center justify-end w-1/2">
-        <div
-        className={`${styles.fadeContainer} ${isFading ? styles.fadeOut : styles.fadeIn} rounded-lg`}
-      >
-        <Image
-        src={gifs[currentGifIndex].src}
-        alt={gifs[currentGifIndex].alt}
-        width={500}
-        height={500}
-        className={`rounded-lg shadow-lg flex justify-end ${styles.img}`}
-      />
-      </div>
+          <div
+            className={`${styles.fadeContainer} ${isFading ? styles.fadeOut : styles.fadeIn} rounded-lg`}
+          >
+            <Image
+              src={gifs[currentGifIndex].src}
+              alt={gifs[currentGifIndex].alt}
+              width={500}
+              height={500}
+              className={`rounded-lg shadow-lg flex justify-end ${styles.img}`}
+            />
+          </div>
         </div>
       </section>
 
       {/* Separator */}
       <div className="h-1 bg-teal-500 my-16 mx-6 rounded-full" />
-
-      {/* Meet the Team Section */}
-      <section className="container mx-auto px-6 py-16">
+      <section className="container mx-auto px-6 py-8">
         <h1 className="text-4xl font-bold text-teal-500 mb-8 text-center">Meet the Team</h1>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
           {teamMembers.map((member) => (
@@ -131,7 +142,22 @@ const AboutPage: React.FC = () => {
               <div className="w-32 h-32 bg-gray-200 rounded-full flex items-center justify-center mb-4">
                 <span className="text-gray-400">Photo</span>
               </div>
-              <h2 className="text-xl font-semibold text-teal-500 mb-2">{member.name}</h2>
+              <h2 className="text-xl font-semibold text-teal-500">{member.name}</h2>
+              <h3 className="text-l text-teal-500 mb-2">{member.role}</h3>
+              <section className="flex justify-center items-center mb-2">
+                <a
+                  className="group flex justify-center p-2 mr-2 rounded-md drop-shadow-xl bg-gradient-to-r from-gray-800 to-black text-white font-semibold hover:rounded-[50%] transition-all duration-500 hover:from-[#331029] hover:to-[#310413]"
+                  href={member.github}
+                >
+                  <FaGithub />
+                </a>
+                <a
+                  className="group flex justify-center p-2 rounded-md drop-shadow-xl bg-[#0077b5] from-gray-800 text-white font-semibold hover:rounded-[50%] transition-all duration-500 hover:from-[#331029] hover:to-[#310413]"
+                  href={member.linkedin}
+                >
+                  <FaLinkedin />
+                </a>
+              </section>
               <p className="text-gray-600 text-center">{member.description}</p>
             </div>
           ))}
